a0cd6610d5dfc244d25c5e82d59c53f529597d54
nivit <nivithas2005@gmail.com>
2026-03-22 18:58:51 +0530

    day 1 class,obj,encapsul

---
+++ day_1/Main.java
@@ -0,0 +1,12 @@
+class Student{
+    int id;
+    String name;
+}
+public class Main{
+    public static void main(String[] args) {
+    Student s1=new Student();
+    s1.id=114;
+    s1.name="nivitha";  
+    System.err.println(s1.id+" "+s1.name);  
+    }
+}
